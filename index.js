dff2651991790cda9ebe876443942223ee5da3ce
Gabriele Petronella <gabriele@buildo.io>
2017-01-14 02:54:49 +0100

    Add support for plugins

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ const yargs = require('yargs');
 const colors = require('colors/safe');
 const { CLIEngine } = require('eslint');
 const git = require('gift');
+const npmi = require('npmi');
 
 type ESLintConfig = {
   rules?: { [_: string]: 0 | 1 | 2 | Object },
-  extends?: string
+  extends?: string,
+  plugins?: Array<string>
 };
 
 type Repo = {
@@ -39,46 +41,87 @@ const log = ({ owner, name }: Repo) => (message: string) => {
 
 tmp.setGracefulCleanup();
 
-function checkRepo(repo: Repo, eslintConfig: ESLintConfig = {}): Promise<void> {
+function makeTmp(): Promise<string> {
   return new Promise((resolve, reject) => {
-    tmp.dir({ unsafeCleanup: true }, (err, path, cleanupCallback) => {
+    tmp.dir({ unsafeCleanup: true }, (err, path) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(path);
+      }
+    });
+  });
+}
+
+function clone(url: string, path: string, depth: number): Promise<string> {
+  return new Promise((resolve, reject) => {
+    git.clone(url, path, depth, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(path);
+      }
+    });
+  });
+}
+
+type InstalledPlugin = {
+  name: string,
+  path: string
+};
+
+function installPlugin(plugin: string, path: string): Promise<InstalledPlugin> {
+  return new Promise((resolve, reject) => {
+    npmi({ path, name: pluginFullName(plugin), npmLoad: { progress: false, loglevel: 'silent' } }, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        const pluginPath = `${path}/node_modules/${pluginFullName(plugin)}`;
+        resolve({ name: pluginFullName(plugin), path: pluginPath });
+      }
+    });
+  });
+}
+
+function installPlugins(plugins: Array<string>): Promise<Array<InstalledPlugin>> {
+  return makeTmp().then(path => {
+    return Promise.all(plugins.map(plugin => installPlugin(plugin, path)));
+  });
+}
+
+function pluginFullName(plugin: string): string {
+  const prefix = 'eslint-plugin-';
+  return plugin.indexOf(prefix) === 0 ? plugin : `${prefix}${plugin}`;
+}
+
+function checkRepo(repo: Repo, eslintConfig: ESLintConfig = {}, installedPlugins: Array<InstalledPlugin>): Promise<Result> {
+  return makeTmp()
+    .then(path => {
       log(repo)(`created tmp directory ${path}`);
       log(repo)('cloning repo...');
       const host = repo.host || 'github.com';
-      git.clone(`git@${host}:${repo.owner}/${repo.name}`, path, 1, (err, r) => {
-        if (err) {
-          log(repo)(err);
-          return;
-        }
-        process.chdir(path);
-        log(repo)(`cloned repo into tmp directory`);
-        try {
-          const defaultBaseConfig = { extends: 'buildo' };
-          const config = {
-            useEslintrc: false,
-            baseConfig: Object.assign({}, defaultBaseConfig, eslintConfig)
-          };
-          const cli = new CLIEngine(config);
-          const files = repo.paths || ['src', 'web/src'];
-          const report = cli.executeOnFiles(files);
-          const formatter = cli.getFormatter('stylish');
-          if (report.errorCount > 0) {
-            log(repo)(formatter(report.results));
-          } else {
-            log(repo)('No style errors!')
-          }
-          resolve({ repo, errorCount: report.errorCount });
-        } catch (e) {
-          log(repo)(e);
-          reject(e);
-        } finally {
-          cleanupCallback();
-          log(repo)('Temp directory cleaned up');
-        }
-      }, e => log(repo)(e));
+      return clone(`git@${host}:${repo.owner}/${repo.name}`, path, 1);
+    })
+    .then(path => {
+      process.chdir(path);
+      log(repo)(`cloned repo into tmp directory`);
+      const defaultBaseConfig = { extends: 'buildo' };
+      const config = {
+        useEslintrc: false,
+        baseConfig: Object.assign({}, defaultBaseConfig, eslintConfig)
+      };
+      const cli = new CLIEngine(config);
+      installedPlugins.forEach(({ name, path }) => cli.addPlugin(name, require(path)));
+      const files = repo.paths || ['src', 'web/src'];
+      const report = cli.executeOnFiles(files);
+      const formatter = cli.getFormatter('stylish');
+      if (report.errorCount > 0) {
+        log(repo)(formatter(report.results));
+      } else {
+        log(repo)('No style errors!')
+      }
+      return { repo, errorCount: report.errorCount };
     });
-  });
-
 }
 
 const argv = yargs.argv;
@@ -90,10 +133,15 @@ if (!argv.config) {
 
 const config = JSON.parse(fs.readFileSync(argv.config, 'utf8'));
 
-Promise.all(config.repos.map(repo => checkRepo(repo, config.eslintConfig))).then(results => {
+installPlugins(config.eslintConfig.plugins || [])
+.then(installedPlugins => {
+  return Promise.all(config.repos.map(repo => checkRepo(repo, config.eslintConfig, installedPlugins).catch(e => log(repo)(e))));
+})
+.then(results => {
   const icon = errorCount => errorCount > 0 ? '⛔️' : '✅';
   const res = results.map(({ repo: { owner, name }, errorCount }) => `|${icon(errorCount)} | ${owner}/${name} | ${errorCount} |`);
   console.log('| |  repo   | errors |');
   console.log('|-|---------|--------|');
   console.log(res.join('\n'));
 });
+
